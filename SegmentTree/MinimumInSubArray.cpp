ae67bf7ed0f3f24ff131d6e4e14ced32464f3a7c
rahuljha661 <rahuljha12527@gmail.com>
2020-07-02 12:47:49 +0530

    Minimum Sub Array

--- SegmentTree/MinimumInSubArray.cpp
+++ SegmentTree/MinimumInSubArray.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
+#include<climits>
+#include<algorithm>
+
 using namespace std;
 
 void buildTree(int *arr,int *tree,int start,int end,int treeNode){
 
     if(start==end){
+      
       tree[treeNode]=arr[start];
       return;
     }
-
+    
     int mid=(start+end)/2;
     buildTree(arr,tree,start,mid,2*treeNode);
     buildTree(arr,tree,mid+1,end,2*treeNode+1);
 
-    tree[treeNode]=tree[2*treeNode]+tree[2*treeNode+1];
+
+    tree[treeNode]=min(tree[2*treeNode],tree[2*treeNode+1]);
 }
+
 void updateTree(int *arr,int *tree,int start,int end,int treeNode,int idx,int value){
+
     if(start==end){
-        arr[idx]=value;
-        tree[treeNode]=value;
-        return;
+       arr[idx]=value;
+       tree[treeNode]=value;
+       return;       
     }
 
     int mid=(start+end)/2;
@@ -29,59 +36,59 @@ void updateTree(int *arr,int *tree,int start,int end,int treeNode,int idx,int va
         updateTree(arr,tree,start,mid,2*treeNode,idx,value);
     }
 
-    tree[treeNode]=tree[2*treeNode]+tree[2*treeNode+1];
-    
+
+    tree[treeNode]=min(tree[2*treeNode],tree[2*treeNode+1]);
+
+
+
 }
 
 int query(int *tree,int start,int end,int treeNode,int left,int right){
-    
+
     if(start>right || end<left){
-        return 0;
+        return INT_MAX;
+
     }
 
-    if(start>=left && end<=right){
-        return tree[treeNode];
+    if(start>=left && end<=right ){
+        return tree[treeNode];  
     }
-
     int mid=(start+end)/2;
-
     int ans1=query(tree,start,mid,2*treeNode,left,right);
     int ans2=query(tree,mid+1,end,2*treeNode+1,left,right);
-
-    return ans1+ans2;
-
+    return min(ans1,ans2);
 }
+
 int main(){
+    int n,q;
+    cin>>n>>q;
+    
 
-    int arr[]={1,2,3,4,5,6,7,8,9};
-    int *tree=new int[18];
-    buildTree(arr,tree,0,8,1);
-
-    updateTree(arr,tree,0,8,1,2,10);
-    for(int i=1;i<18;i++){
-        cout<<tree[i]<<endl;
+    int *arr=new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-     
-    int ans=query(tree,0,4,1,2,4);
 
-   cout<<ans<<endl;   
+    int *tree=new int[4*n];
+    buildTree(arr,tree,0,n-1,1);
+
+    while(q--){
+        char query_T;
+         cin>>query_T;
+        if(query_T=='q'){
+          int l,r;
+          cin>>l>>r;
+          
+          cout<<query(tree,0,n-1,1,l-1,r-1)<<endl;
+        }
+        else{
+           int x,y;
+           cin>>x>>y;
+           
+           updateTree(arr,tree,0,n-1,1,x-1,y);
+        }
+
+    }
+
+
 }
-
-// 52
-// 22
-// 30
-// 13
-// 9
-// 13
-// 17
-// 3
-// 10
-// 4
-// 5
-// 6
-// 7
-// 8
-// 9
-// 1
-// 2
-// 39
